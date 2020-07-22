ad540386cc9c37c5068f98aba6ee8e5476ef3a5c
Craig Cronin <craig,cronin@gmail.com>
2020-07-22 14:36:56 -0400

    Roll version

--- screens/Profile.js
+++ screens/Profile.js
@@ -11,7 +11,7 @@ import { ATTEMPT_LOGOUT } from '../redux/actions/types'
 
 import ProfileHeader from '../components/ProfileHeader'
 
-const VERSION = '1.2.4'
+const VERSION = '1.2.5'
 
 const styles = StyleSheet.create({
   activityContainer: {
